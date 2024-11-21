b723998e8c482f7fb9afaca4d2f5ef59166d519d
Bisma Muqeed <bismamuqeed@gmail.com>
2024-11-21 06:05:51 +0500

    Assignment 5 is completed

--- src/app/Component/Header.tsx
+++ src/app/Component/Header.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import style from "next/font/google";
+
 
 const Header = () => {
     return (
-      <div className="bg-[#A29875] w-[1920px] h-[134px] flex items-center justify-start box-border pl-[36px] pt-[20px] overflow-x-hidden">
-        <h1 className= "font-[400] text-[75px] leading-[93.75px font-rye w-[439px] h-[94px] font-['Rye'] text-white overflow-[none]">
-        
-          MANZZARI
-        </h1>
-      </div>
+      <div className="bg-[#A29875]  w-[1920px] h-[134px] overflow-x-hidden">
+     <h1 className = "w-[439px] h-[94px] absolute top-[20px] left-[36px] font-['Rye'] text-[75px] leading-[93.75px] font-[400] text-[#FFFFFF] ">MANZZARI</h1> 
+  </div>     
+    
+    
     );
   };
   
